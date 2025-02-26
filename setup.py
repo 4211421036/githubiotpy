7064036523057c82fc9350caa698031c7b4a183c
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-02-26 21:20:19 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="githubiot",
     version="1.0.2",
-    author="GALIH RIDHO UTOMO, Fionita Fahra Azzahra",
-    author_email="g4lihru@students.unnes.ac.id, fionitafahra13@students.unnes.ac.id",
+    author= GALIH RIDHO UTOMO, Fionita Fahra Azzahra,
+    author_email= g4lihru@students.unnes.ac.id, fionitafahra13@students.unnes.ac.id,
     description="GitHubIoT is a comprehensive toolkit designed to simplify the visualization of IoT (Internet of Things) data with seamless GitHub integration. The application provides an intuitive graphical interface for real-time data monitoring, analysis, and configuration, making it ideal for both beginners and experienced developers working with IoT devices microcontroler (ESP32 or ESP8266) realtime",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/4211421036/githubiotpy",
+    keyword= Github, IoT, Arduino IDE, Python Modules, ESP32, ESP8266, webApp
     packages=find_packages(),
     include_package_data=True,
     package_data={
@@ -33,7 +34,6 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Topic Software Development :: Libraries :: Python Modules",
     ],
     python_requires=">=3.6",
 )
