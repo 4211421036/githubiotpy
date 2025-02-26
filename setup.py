27eec51bd64d18167cbe9da59cd1489440713aae
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-02-26 18:33:00 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,38 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name='githubiot',
-    version='1.0.0',
-    py_modules=['githubiot_cli'],
+    name="githubiot",
+    version="1.0.0",
+    author="GALIH RIDHO UTOMO",
+    author_email="g4lihru@students.unnes.ac.id",
+    description="IoT Visualization Toolkit with GitHub Integration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/4211421036/githubiotpy",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "githubiot": ["templates/*"],
+    },
     install_requires=[
-        'requests',
-        'matplotlib',
-        'pyinstaller'
+        "requests>=2.26.0",
+        "matplotlib>=3.5.0", 
+        "numpy>=1.21.0",
+        "pyinstaller>=5.0.0",
     ],
     entry_points={
-        'console_scripts': [
-            'githubiot=githubiot_cli:main',
+        "console_scripts": [
+            "githubiot=githubiot.cli:run_cli",
         ],
     },
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Microsoft :: Windows',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires=">=3.6",
 )
